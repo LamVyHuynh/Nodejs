6aeb4ae8c4c315d2186e861487d1c5ae139152d1
LamVyHuynh <huynhlamvy145@gmail.com>
2025-02-13 10:42:35 +0700

    bt5

---
+++ bt5.js
@@ -0,0 +1,18 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function namNhuan(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    console.log("Năm nhuận");
+  } else {
+    console.log("Không phải năm nhuận");
+  }
+}
+rl.question("Nhập số năm:", (year) => {
+  year = parseInt(year);
+  namNhuan(year);
+  rl.close();
+});
