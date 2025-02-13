447956abdda877da9cf21546307dc1bcbdd0941f
LamVyHuynh <huynhlamvy145@gmail.com>
2025-02-13 15:19:43 +0700

    bt6 xong

---
+++ bt6.js
@@ -0,0 +1,22 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Nhập điểm vào: ", (diem) => {
+  diem = parseInt(diem);
+  kTraDiem(diem);
+  rl.close();
+});
+function kTraDiem(diem) {
+  if (diem >= 90) {
+    console.log("Xuất sắc");
+  } else if (diem < 90 && diem >= 80) {
+    console.log("Giỏi");
+  } else if (diem < 80 && diem >= 70) {
+    console.log("Khá");
+  } else {
+    console.log("Trung bình");
+  }
+}
