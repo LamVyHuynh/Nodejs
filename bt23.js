68cf792ed6211452f774ca855bcf09a1f16b7d11
LamVyHuynh <huynhlamvy145@gmail.com>
2025-02-13 23:24:01 +0700

    sua lai bt23 va bt25 xong

--- bt23.js
+++ bt23.js
@@ -1,4 +1,6 @@
 const a = "5";
 const b = 5;
+// Output: true (chỉ so sánh giá trị)
 console.log(a == b);
+// Output: false (so sánh cả giá trị và kiểu dữ liệu
 console.log(a === b);
---
+++ bt25.js
@@ -0,0 +1,7 @@
+const num = 5.7;
+// Làm tròn gần nhất
+console.log(Math.round(num));
+// Làm tròn xuống
+console.log(Math.floor(num));
+// Làm tròn lên
+console.log(Math.ceil(num));
