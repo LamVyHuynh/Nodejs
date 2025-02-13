a20aed231f58b3849da5589aca2732d6c587d239
LamVyHuynh <huynhlamvy145@gmail.com>
2025-02-13 22:16:50 +0700

    bt15 xong

---
+++ bt15.js
@@ -0,0 +1,23 @@
+const readline = require("readline");
+const rl = readline.Interface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function soNguyenTo(n) {
+  let isPrime = true;
+  if (n < 2) isPrime = false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  console.log(isPrime ? "Số nguyên tố" : "Không là số nguyên tố");
+}
+
+rl.question("Nhập số n:", (n) => {
+  n = parseInt(n);
+  soNguyenTo(n);
+  rl.close();
+});
